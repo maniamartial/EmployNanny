4a624667e5713601efc3c0df13dab545bd97314f
maniamartial <martiallmania19@gmail.com>
2023-04-13 19:14:28 +0300

    Making nanny details form

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import NannyDetails
 
-# Register your models here.
+class NannyAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'city', 'language')
+
+admin.site.register(NannyDetails, NannyAdmin)
