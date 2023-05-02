e6a30af821ae6f4b1a4e6fcd89c79310513c3f79
martialmania <martialmania19@gmail.com>
2023-05-02 19:16:27 +0300

    Forgotten passwords

--- Employnannys/urls.py
+++ Employnannys/urls.py
@@ -18,6 +18,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,32 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('chat/', include('messaging.urls')),
     path('payment/', include('payment.urls')),
+
+
+    # authentication
+    # Password reset form view.
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset.html'),
+         name='password_reset'),
+
+    # Password reset done view.
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+
+    # Password reset confirmation view.
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+
+    # Password reset complete view.
+    path('password-reset-complete/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
