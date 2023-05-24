3a53414294f2acee35f44e993ebda6a3b6304eb3
martialmania <martialmania19@gmail.com>
2023-05-24 19:01:57 +0300

    Chat page with all members

--- messaging/views.py
+++ messaging/views.py
@@ -13,6 +13,7 @@ def room(request, room_name):
     receiver = get_user(request)  # Get the receiver, who is the logged-in user
     return render(request, "chat/room.html", {"room_name": room_name, "receiver": receiver})
 '''
+from django.db.models import Max
 import uuid  # Import the uuid module
 from django.shortcuts import get_object_or_404
 from .models import Message
@@ -53,4 +54,24 @@ def room(request, room_name, receiver_id):
     messages = Message.objects.filter(sender=sender, receiver=receiver).order_by('timestamp') | Message.objects.filter(
         sender=receiver, receiver=sender).order_by('timestamp')
 
+    # Mark unread messages as seen for the receiver
+    unread_messages = messages.filter(receiver=receiver, is_seen=False)
+    print(unread_messages)
+    if unread_messages.exists() and request.user == receiver:
+        unread_messages.update(is_seen=True)
+
     return render(request, "chat/room.html", {"room_name": room_name, "receiver": receiver, "messages": messages})
+
+
+def chat_list(request):
+    user = request.user
+
+    # Retrieve the latest message for each sender
+    latest_messages = Message.objects.filter(receiver=user).values(
+        'sender').annotate(max_timestamp=Max('timestamp'))
+
+    # Fetch the complete message objects based on the latest messages
+    chats = Message.objects.filter(receiver=user, sender__in=latest_messages.values(
+        'sender'), timestamp__in=latest_messages.values('max_timestamp'))
+
+    return render(request, 'chat/chat_list.html', {'chats': chats})
