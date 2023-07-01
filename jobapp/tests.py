92ab12f14de8a52fab2b4c75262593ad43b81e1f
martialmania <martialmania19@gmail.com>
2023-07-01 17:13:30 +0300

    Contract and job application tests

--- jobapp/tests.py
+++ jobapp/tests.py
@@ -1,10 +1,17 @@
-from django.http import QueryDict
+from jobapp.models import ContractModel, JobApplication
 from django.contrib.messages import get_messages
+import datetime as datetime
+from django.core import mail
+from .models import JobApplication, ContractModel, jobModel
+from django.test import TestCase
+from django.shortcuts import get_object_or_404
+from users.models import NannyDetails, EmployerProfile
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse
 from .models import jobModel
 from .form import jobPostingForm
+from Notifications.models import Notification
 
 
 class JobPostingTestCase(TestCase):
@@ -205,3 +212,341 @@ class EditJobTestCase(TestCase):
         self.assertEqual(updated_job.language, updated_data['language'])
         # Assert that the category remains unchanged
         self.assertEqual(updated_job.category, 'Full-time Nanny')
+
+
+# test applying for a job
+
+
+class ApplyForJobTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.job = jobModel.objects.create(
+            employer=self.user,
+            category='Full-time Nanny',
+            city='Test City',
+            addresss='Test Address',
+            salary=23490,
+            language='Test Language',
+            nanny_age='18-25',
+            hours_per_day=2,
+            start_date='2023-06-29',
+            years_of_experience=0,
+            duration='1 - 2 Years',
+            days_off='Every weekend',
+            leave_available=False,
+            vacation_days_per_year='2 weeks',
+            job_description='Test Job Description'
+        )
+        self.nanny_details = NannyDetails.objects.create(
+            user=self.user,
+            first_name='Test',
+            last_name='Nanny',
+            # Add other necessary nanny details
+        )
+
+    def test_apply_for_job(self):
+        # Log in the user
+        self.client.login(username='testuser', password='testpassword')
+
+        # Send a POST request to apply for the job
+        response = self.client.post(
+            reverse('apply_for_job', args=[self.job.pk]))
+
+        # Check for a redirect response
+        self.assertEqual(response.status_code, 302)
+
+        # Check if a JobApplication object is created
+        job_application = JobApplication.objects.filter(
+            job=self.job, nanny=self.nanny_details).first()
+        self.assertIsNotNone(job_application)
+
+        # Assert the success message
+        messages = list(response.wsgi_request._messages)
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(
+            str(messages[0]), 'Your job application has been submitted successfully.')
+
+        # Add more assertions to check if the email notification and employer notification are sent correctly
+
+        # Redirect to the appropriate page
+        self.assertRedirects(response, reverse('job_application_status'))
+
+
+# create Contract
+
+
+class CreateContractTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.nanny = NannyDetails.objects.create(user=self.user)
+        self.employer_profile = EmployerProfile.objects.create(user=self.user)
+
+        self.job = jobModel.objects.create(
+            employer=self.employer_profile.user,
+            category='Full-time Nanny',
+            city='Your City',
+            addresss='Your Address',
+            salary=1000,
+            language='English',
+            nanny_age='18-25',
+            hours_per_day=8,
+            start_date=datetime.date.today(),
+            years_of_experience=3,
+            duration='1 - 2 years',
+            job_description='Your job description',
+            days_off='Every weekend',
+            leave_available=False,
+            vacation_days_per_year='2 weeks'
+        )
+
+        self.application = JobApplication.objects.create(
+            job=self.job, nanny=self.nanny
+        )
+
+    def test_create_contract_and_start_duration(self):
+        login_successful = self.client.login(
+            username='testuser',
+            password='testpassword'
+        )
+        self.assertTrue(login_successful)
+
+        response = self.client.get(
+            reverse('create_contract', args=[self.application.id])
+        )
+
+        # Check for a successful response
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the contract has been created
+        contract = ContractModel.objects.filter(
+            job=self.job, nanny=self.nanny, employer=self.employer_profile.user
+        ).first()
+        self.assertIsNotNone(contract)
+
+        # Check if the contract start date is set
+        self.assertIsNotNone(contract.start_date)
+
+        # Check if the application status has been updated
+        self.assertEqual(self.application.status, 'pending')
+
+        # Check if the notification has been saved to the nanny's model
+        nanny_notification = Notification.objects.filter(
+            user=self.nanny.user
+        ).first()
+        self.assertIsNotNone(nanny_notification)
+        self.assertEqual(nanny_notification.title, 'New Contract')
+
+        # Add more assertions as needed
+
+        # Clean up the test data
+        contract.delete()
+        self.application.delete()
+        self.job.delete()
+        self.employer_profile.delete()
+        self.nanny.delete()
+        self.user.delete()
+
+
+# Test if contract will get accepted or rejected
+class AcceptContractTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.nanny = NannyDetails.objects.create(user=self.user)
+        self.employer_profile = EmployerProfile.objects.create(user=self.user)
+        self.job = jobModel.objects.create(
+            employer=self.employer_profile.user,
+            category='Full-time Nanny',
+            city='Your City',
+            addresss='Your Address',
+            salary=1000,
+            language='English',
+            nanny_age='18-25',
+            hours_per_day=8,
+            start_date=datetime.date.today(),
+            years_of_experience=3,
+            duration='1 - 2 years',
+            job_description='Your job description',
+            days_off='Every weekend',
+            leave_available=False,
+            vacation_days_per_year='2 weeks'
+        )
+        self.application = JobApplication.objects.create(
+            job=self.job, nanny=self.nanny
+        )
+        self.contract = ContractModel.objects.create(
+            job=self.job, nanny=self.nanny, employer=self.employer_profile.user
+        )
+
+    def test_accept_contract(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('accept_contract', args=[self.contract.id]), {
+            'accept': 'Accept',
+            'signature_data': 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAADICAYAAACtWK6eAAAgAElEQVR4XuzdC6RUZdvH8VJ+iYr5...',
+        })
+
+        # Check for a successful response
+        self.assertEqual(response.status_code, 302)
+
+        # Refresh the contract object from the database
+        self.contract.refresh_from_db()
+
+        # Check if the contract status is updated to 'active'
+        self.assertEqual(self.contract.status, 'active')
+
+        # Refresh the job application object from the database
+        self.application.refresh_from_db()
+
+        # Check if the job application status is updated to 'accepted'
+        self.assertEqual(self.application.status, 'accepted')
+
+        # Check if a success message is added to the messages framework
+        messages = [str(m) for m in get_messages(response.wsgi_request)]
+        self.assertIn('Contract accepted successfully.', messages)
+
+        # Add more assertions as needed
+
+    def tearDown(self):
+        self.contract.delete()
+        self.application.delete()
+        self.job.delete()
+        self.employer_profile.delete()
+        self.nanny.delete()
+        self.user.delete()
+
+
+# delete job application
+
+
+class DeleteJobApplicationTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.nanny = NannyDetails.objects.create(user=self.user)
+        self.employer_profile = EmployerProfile.objects.create(user=self.user)
+        self.job = jobModel.objects.create(
+            employer=self.employer_profile.user,
+            category='Full-time Nanny',
+            city='Your City',
+            addresss='Your Address',
+            salary=1000,
+            language='English',
+            nanny_age='18-25',
+            hours_per_day=8,
+            start_date=datetime.date.today(),
+            years_of_experience=3,
+            duration='1 - 2 years',
+            job_description='Your job description',
+            days_off='Every weekend',
+            leave_available=False,
+            vacation_days_per_year='2 weeks'
+        )
+        self.application = JobApplication.objects.create(
+            job=self.job, nanny=self.nanny
+        )
+
+    def test_delete_job_application(self):
+        self.client.force_login(self.user)
+        response = self.client.post(
+            reverse('delete_job_application', args=[self.application.id]))
+
+        # Check for a successful response
+        self.assertEqual(response.status_code, 302)
+
+        # Check if the job application is deleted from the database
+        self.assertFalse(JobApplication.objects.filter(
+            id=self.application.id).exists())
+
+        # Check if a success message is added to the messages framework
+        messages = [str(m) for m in get_messages(response.wsgi_request)]
+        self.assertIn('Job application deleted successfully.', messages)
+
+        # Add more assertions as needed
+
+    def tearDown(self):
+        self.application.delete()
+        self.job.delete()
+        self.employer_profile.delete()
+        self.nanny.delete()
+        self.user.delete()
+
+
+class EndContractTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.nanny = User.objects.create_user(
+            username='nannyuser',
+            password='nannypassword'
+        )
+        self.employer_profile = EmployerProfile.objects.create(user=self.user)
+        self.nanny_details = NannyDetails.objects.create(user=self.nanny)
+        self.job_application = JobApplication.objects.create(
+            job=jobModel.objects.create(
+                employer=self.employer_profile.user,
+                category='Full-time Nanny',
+                city='Your City',
+                addresss='Your Address',
+                salary=1000,
+                language='English',
+                nanny_age='18-25',
+                hours_per_day=8,
+                start_date=datetime.date.today(),
+                years_of_experience=3,
+                duration='1 - 2 years',
+                job_description='Your job description',
+                days_off='Every weekend',
+                leave_available=False,
+                vacation_days_per_year='2 weeks'
+            ),
+            nanny=self.nanny_details
+        )
+        self.contract = ContractModel.objects.create(
+            id=1,
+            job=self.job_application.job,
+            nanny=self.nanny_details
+        )
+
+    def test_end_contract(self):
+        # Log in as the employer
+        self.client.login(username='testuser', password='testpassword')
+
+        # Send a POST request to end the contract
+        response = self.client.post(
+            reverse('end_contract', args=[self.contract.id]),
+            follow=True
+        )
+
+        # Check if the contract and job application have been terminated
+        self.contract.refresh_from_db()
+        self.assertEqual(self.contract.status, 'terminated')
+        self.job_application.refresh_from_db()
+        self.assertEqual(self.job_application.status, 'terminated')
+
+        # Check if a notification has been saved for the nanny
+        nanny_notification = Notification.objects.filter(
+            user=self.nanny_details.user).first()
+        self.assertIsNotNone(nanny_notification)
+        self.assertEqual(nanny_notification.title, 'Give review')
+
+        # Check if the user is redirected to the post-review page
+        self.assertRedirects(response, reverse(
+            'post_review', args=[self.contract.id]))
+
+        # Check the success message
+        messages = [str(m) for m in response.context['messages']]
+        self.assertIn('Contract terminated successfully.', messages)
